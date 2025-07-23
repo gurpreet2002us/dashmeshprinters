0009d01a5f6e76f5c4e14d9576c7004fe788cd39
guru0777 <dashmeshprinters50@gmail.com>
2025-07-23 08:10:55 +0530

    twitter link removed from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,9 +26,9 @@ const Footer = () => {
                 <a href="#" className="w-10 h-10 bg-primary rounded-full flex items-center justify-center hover:bg-primary-light transition-colors">
                   <Instagram className="h-5 w-5" />
                 </a>
-                <a href="#" className="w-10 h-10 bg-primary rounded-full flex items-center justify-center hover:bg-primary-light transition-colors">
+                {/* <a href="#" className="w-10 h-10 bg-primary rounded-full flex items-center justify-center hover:bg-primary-light transition-colors">
                   <Twitter className="h-5 w-5" />
-                </a>
+                </a> */}
               </div>
             </div>
 
