f82ae6d401360f388b0760051d7de986a1fa5912
guru0777 <dashmeshprinters50@gmail.com>
2025-07-23 16:14:53 +0530

    changes push

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
                 </a>
                 {/* <a href="#" className="w-10 h-10 bg-primary rounded-full flex items-center justify-center hover:bg-primary-light transition-colors">
                   <Twitter className="h-5 w-5" />
-                </a> */}
+                </a>  */}
               </div>
             </div>
 
